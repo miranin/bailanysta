a594fc8497970f9c11a3d226e79300734168d59b
tamerlan.aubaqirov <tamerlan.aubaqirov@octogroup.uz>
2025-09-05 15:24:54 +0500

    Initial Commit

---
+++ Bailanysta/BailanystaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BailanystaApp.swift
+//  Bailanysta
+//
+//  Created by Tamirlan Aubakirov on 05/09/25.
+//
+
+import SwiftUI
+
+@main
+struct BailanystaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bailanysta/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bailanysta
+//
+//  Created by Tamirlan Aubakirov on 05/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
